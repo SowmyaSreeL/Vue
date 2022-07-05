4c7f5d2ddf132efa1cdfba1d1bcdd6db29d60514
Sowmya Sree Lagishetti <sowmya.sree@tadigital.com>
2022-07-05 18:35:41 +0530

    slots

---
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals>
+       <!-- We can use v-slot:default="slotProps" instead of the shorthand #default -->
+      <template #default="slotProps">
+        <h2>{{slotProps.item}}</h2>
+        <p>{{slotProps.anotherProp}}</p>
+      </template>
+    </course-goals>
+  </div>
+</template>
+
+<script>
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+import TheHeader from './components/TheHeader.vue';
+import CourseGoals from './components/CourseGoals.vue'
+export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+    CourseGoals
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <slot :item="goal" another-prop=".....dummy"></slot>
+        </li>
+    </ul>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            goals: ['FInish the course', 'Learnt Vue']
+        }
+    },
+}
+</script>
